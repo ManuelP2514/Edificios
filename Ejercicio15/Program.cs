1575ac09da973990871aa21215cd5f27956b9dd8
manue <manue@LAPTOP-TKTP7A3B>
2022-07-05 01:12:11 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Pisos, AlturaDeCadaPiso, AlturaEdificio;
+            Console.WriteLine("Ingrese la cantidad de Pisos ");
+            Pisos = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la altura de cada Piso ");
+            AlturaDeCadaPiso = double.Parse(Console.ReadLine());
+            AlturaEdificio = (Pisos * AlturaDeCadaPiso);
+            Console.WriteLine($"La Altura Del Edificio Es De {AlturaEdificio} Metros");
+        }
+    }
+}
